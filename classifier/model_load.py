c4580ffd99d36b07b308d1cd5b7adae135b823bf
raflidzaky <raflidzakyansyah>
2024-10-01 22:12:41 +0700

    Add return statement for better func

--- classifier/model_load.py
+++ classifier/model_load.py
@@ -1,8 +1,13 @@
 import torch
 
 def model_loader(model_path, model):
-    # Load the checkpoint from the specified path
-    checkpoint  = torch.load(model_path)
-    
-    # Rebuild the classifier using the loaded parameters
-    model.load_state_dict(checkpoint['model_state_dict'])
+    try:
+        # Load the checkpoint from the specified path
+        checkpoint = torch.load(model_path)
+        
+        # Rebuild the classifier using the loaded parameters
+        model.load_state_dict(checkpoint['model_state_dict'])
+        return model
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        return None
