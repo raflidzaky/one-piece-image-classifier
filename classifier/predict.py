b360f1c7235f22bc4ca132cd6942707135e921d7
raflidzaky <raflidzakyansyah>
2024-10-01 22:16:05 +0700

    Update prediction return

--- classifier/predict.py
+++ classifier/predict.py
@@ -1,14 +1,29 @@
-# TODO: 
-# 1. Make a func to extract class names --> match logits as an extract names
-# 2. Make a func to do some prediction which return image labels and its probability
-# The flow is here:
-# Image is being uploaded and displayed --> API sent it to the pre-processing --> API sent the image tensors to predict
-# API sent the predicted label and probability back to landing page...
-
 import torch
-from image_preprocess import process_image
-from model_build import model
-from model_load import model_loader
+from image_preprocess import image_transform, image_processing
+
+# Categories to map
+# Since the class is simple, I think we no need to store it as a .txt yet
+# I store as a dict to access it with class index in prediction
+categories = [
+    "Ace",
+    "Akainu",
+    "Brook",
+    "Chopper",
+    "Crocodile",
+    "Franky",
+    "Jinbei",
+    "Kurohige",
+    "Law",
+    "Luffy",
+    "Mihawk",
+    "Nami",
+    "Rayleigh",
+    "Robin",
+    "Sanji",
+    "Shanks",
+    "Usopp",
+    "Zoro"
+]
 
 def predict(input, model):
     ''' 
@@ -17,7 +32,7 @@ def predict(input, model):
     model.eval()
     with torch.no_grad():
         # Process the image
-        img = process_image(input)
+        img = image_processing(input=input, transform=image_transform())
         
         # Predict prob and class
         logits = model(img)
@@ -26,4 +41,4 @@ def predict(input, model):
         top_pr, top_class = prob.topk(k=1, dim=1)
         
         # Convert the indices of top_k to its real name
-        return top_pr.item(), top_class.item()
+        return top_pr.item(), categories[top_class.item()]
