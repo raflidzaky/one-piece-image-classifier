ef533237e0c585f66bb50ca604b38ee6527112e6
raflidzaky <raflidzakyansyah>
2024-10-04 00:02:13 +0700

    Add documentation

--- streamlit_app/display.py
+++ streamlit_app/display.py
@@ -2,6 +2,15 @@ import streamlit as st
 from PIL import Image
 import display_image
 
+'''
+    This module has several functions that provide utilities to support basic landing page,
+    as users entered the web. Several utilities:
+    1. What page that users currently in
+    2. Guide for users 
+    3. Upload image button, that users could upload to
+    4. Button for prediction
+'''
+
 def display_page():
     # Func for landing page introduction
     st.title("Ahoy! Welcome to One Piece Image Recognizer!")
@@ -22,9 +31,15 @@ def button():
     image = st.file_uploader('Upload image here', type=['png', 'jpg', 'jpeg'],
                              accept_multiple_files=False,
                              key='up145'
-                            )      
+                            )
+    # Since I want pass a value for the model, this function
+    # will return the image that is being uploaded.
+    # Also, this image will be displayed in display_image.py module.    
     return image
 
 def predict_button():
     st.write("Do you want to know who is he/she?")
+    # Since I want the predict button returns any value after "clicking action"
+    # I make the st.button as return.
+    # Hence, this value will be passed as a logic to trigger steps to predict model.
     return st.button("Yes, I want!", key="predict_button")
