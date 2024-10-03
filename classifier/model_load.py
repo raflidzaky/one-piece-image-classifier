7f632cacd6af919488998b439d2504cabf8d6e89
raflidzaky <raflidzakyansyah>
2024-10-04 00:16:49 +0700

    Delete try except case in production

--- classifier/model_load.py
+++ classifier/model_load.py
@@ -5,14 +5,10 @@ def model_loader(model_path, model):
         This function will load weights to classifier.model_load's architecture. However, to notify bugs, in development,
         I use "try and except". For production, I will not include try and except logic.
     '''
-    try:
         # Load the checkpoint from the specified path
         # Since I only need the weights, I set the loader to weights only.
-        checkpoint = torch.load(model_path, weights_only=True)
+    checkpoint = torch.load(model_path, weights_only=True)
         
         # Rebuild the classifier using the loaded parameters
-        model.load_state_dict(checkpoint['model_state_dict'])
-        return model
-    except Exception as e:
-        print(f"Error loading model: {e}")
-        return None
+    model.load_state_dict(checkpoint['model_state_dict'])
+    return model
