cf3fd9693472979dd03260c255f1452e4a363c34
raflidzaky <raflidzakyansyah>
2024-10-03 23:53:17 +0700

    Add documentation and update edge-case condition

--- streamlit_app/character_script.py
+++ streamlit_app/character_script.py
@@ -1,9 +1,21 @@
 import streamlit as st
+import json
 
+def json_parser():
+    # JSON's parser must met utf-8 encoding
+    with open('C:/Users/Rafli/one_piece_image_classifier/character_info.json', 'r', encoding='utf-8') as f:
+        description = json.load(f)
+        return description 
+    
 def character_script(input, input_prob):
+    description = json_parser()
+
+    # Here, I tried to get key that matches the input arg (top_class from predict.py)
+    char_info   = description.get(input)
     if input_prob > 0.6:
         st.markdown(f"## This is {input}") 
-        st.write('This is a character from one piece')
+        if char_info:
+            st.write(char_info["info"])
     else:
         st.markdown(f"## Sorry!")
         st.write('We are uncertain who is this')
