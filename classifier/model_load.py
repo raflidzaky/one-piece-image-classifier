c00b2979e03e55ebcfc1bba21b3f3b3c6c81e632
raflidzaky <raflidzakyansyah>
2024-10-04 00:14:35 +0700

    Add documentation

--- classifier/model_load.py
+++ classifier/model_load.py
@@ -1,9 +1,14 @@
 import torch
 
 def model_loader(model_path, model):
+    '''
+        This function will load weights to classifier.model_load's architecture. However, to notify bugs, in development,
+        I use "try and except". For production, I will not include try and except logic.
+    '''
     try:
         # Load the checkpoint from the specified path
-        checkpoint = torch.load(model_path)
+        # Since I only need the weights, I set the loader to weights only.
+        checkpoint = torch.load(model_path, weights_only=True)
         
         # Rebuild the classifier using the loaded parameters
         model.load_state_dict(checkpoint['model_state_dict'])
