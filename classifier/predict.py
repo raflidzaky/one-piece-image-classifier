9d39fc51d0e4d3f59e870e5556d1b5f559adcbe7
raflidzaky <raflidzakyansyah>
2024-10-04 00:10:30 +0700

    Modify documentation

--- classifier/predict.py
+++ classifier/predict.py
@@ -1,5 +1,5 @@
 import torch
-from image_preprocess import image_transform, image_processing
+from classifier.image_preprocess import image_transform, image_processing
 
 # Categories to map
 # Since the class is simple, I think we no need to store it as a .txt yet
@@ -29,6 +29,18 @@ categories = [
 def predict(input, model):
     ''' 
         Predict the class of an image using a trained deep learning model.
+        Before returning any prediction, it will load and process the image (using image_preprocess.image_processing)
+        and model (using model_build.model for foundational architecture and model_load.model_loader for the weights)
+        Input: 
+            1. input is the image that users have uploaded in display.button(). Since the image_processing "inherits"
+               image_preprocess.image_transform functionality (as I include it within the image_processing), hence no 
+               need to call image_transform here.
+            2. model that is being used -- basic Convolutional Neural Network architecture.
+        Output: 
+            1. Top category that is limited to 1 class, since this web just only display a specific character and its 
+               information
+            2. Top category's probability. This will be used in streamlit_app.character_script to manage edge-cases.
+
     '''
     model.eval()
     with torch.no_grad():
