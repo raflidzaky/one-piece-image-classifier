81f8d8a91f010e8e94a5c74cb88034f544aef2f9
raflidzaky <raflidzakyansyah>
2024-10-03 23:55:25 +0700

    Add documentation

--- streamlit_app/display_image.py
+++ streamlit_app/display_image.py
@@ -2,6 +2,10 @@ from PIL import Image
 import io
 
 def display_image(image_file):
+    '''
+        This function will return the file as an image (that will be displayed).
+        The input itself is an image that users have submitted.
+    '''
     image_bytes = image_file.read()    
     image = Image.open(io.BytesIO(image_bytes))
     return image
